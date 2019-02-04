59bdd66f7dd9431ebc39854a6a83d419c5f71df9
george <geomarray@gmail.com>
2019-02-03 23:10:03 -0800

    apis working and rendering results, still troubleshooting the trails api

--- src/components/meetup/meetup.js
+++ src/components/meetup/meetup.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Template from './template';
 
 class Meetup extends React.Component {
- return(){
+ render(){
+    let lineItems = []
+    console.log(`☣️ ${this.props.data}`)
+    for(let i = 0; i < this.props.data.length; i++){
+      lineItems.push(<Template data={this.props.data[i]} />);
+    }
    return(
     <section>
       <h3>Results from the Meetup API</h3>
-      <ul className="meetups-results"></ul>
+      <ul className="meetups-results">
+        {lineItems}
+      </ul>
     </section>
+
    );
   }
 }
--- src/components/yelp/yelp.js
+++ src/components/yelp/yelp.js
@@ -4,10 +4,16 @@ import Template from './template';
 
 class Yelp extends React.Component {
   render(){
+    let lineItems = []
+    for(let i = 0; i < this.props.data.length; i++){
+      lineItems.push(<Template data={this.props.data[i]} />);
+    }
     return(
       <section className="yelp-container">
         <h3>Results from the Yelp API</h3>
-        <ul className="yelp-results"></ul>
+        <ul className="yelp-results">
+          {lineItems}
+        </ul>
       </section>
     );
   }
